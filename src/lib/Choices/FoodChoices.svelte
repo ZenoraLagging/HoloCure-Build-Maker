1351a7de6d29ebb834999f45d9437a8603c1763a
zenora <zenoralagging@gmail.com>
2024-11-16 07:36:09 -0800

    Update FoodChoices.svelte

--- src/lib/Choices/FoodChoices.svelte
+++ src/lib/Choices/FoodChoices.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { Button } from "$lib/components/ui/button";
 	import { equippedFoodDish } from "$lib/stores";
-
+	import { images } from "$lib/images/exports.svelte";
 	import { foods } from "$lib/variables";
 
 	/** @type {{display: boolean}} */
