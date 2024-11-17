0ffa6c2beb9fe0e439f4116a5c184d1d2f6e4a0e
zenora <zenoralagging@gmail.com>
2024-11-16 17:54:23 -0800

    Update BuildName.svelte

--- src/lib/Frame/BuildName.svelte
+++ src/lib/Frame/BuildName.svelte
@@ -43,6 +43,6 @@
 		}}
 		onfocus={(e: any) => focusHandler(e.currentTarget.value)}
 		onblur={(e: any) => blurHandler(e.currentTarget.value)}
-		class="text-3xl lg:text-5xl break-all text-left lg:text-center border-none overflow-hidden min-h-10 h-16 max-h-32 bg-opacity-0 z-1 mb-4"
+		class="text-4xl lg:text-5xl break-words text-left lg:text-center border-none overflow-hidden min-h-10 h-16 max-h-32 bg-opacity-0 z-1 mb-4"
 	/>
 {/if}
