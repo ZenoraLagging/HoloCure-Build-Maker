53e1287ce4b5f3377ed08aac1b4ee5c34269bf3b
zenora <zenoralagging@gmail.com>
2024-11-17 15:36:46 -0800

    fix duplicate layouts appearing

--- src/routes/(build)/build/+layout.svelte
+++ src/routes/(build)/build/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import "../../app.css";
+	import "../../../app.css";
 	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
 	import AppSidebar from "$lib/components/app-sidebar.svelte";
 	import { type Snippet } from "svelte";
@@ -18,27 +18,9 @@
 </script>
 
 <svelte:head>
-	{#if $page.data.meta}
-		{#each $page.data.meta as { name, content }}
-			<meta {name} {content} />
-		{/each}
-	{/if}
-
 	<meta property="og:image" content={image} />
 	<meta property="og:site_name" content="HoloCure Build Maker" />
 	<meta property="og:url" content={url} />
 </svelte:head>
 
-<Sidebar.Provider>
-	<AppSidebar />
-	<Sidebar.Inset>
-		<header class="flex mt-2 shrink-0 items-center gap-2">
-			<div class="flex flex-1 items-center gap-2 px-3">
-				<Sidebar.Trigger />
-			</div>
-		</header>
-		<div class="flex flex-1 flex-col px-4">
-			{@render children?.()}
-		</div>
-	</Sidebar.Inset>
-</Sidebar.Provider>
+{@render children?.()}
