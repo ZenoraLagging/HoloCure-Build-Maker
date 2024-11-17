d9731d74741453bbdc2df4eb378e585b893d9c6e
zenora <zenoralagging@gmail.com>
2024-11-17 01:38:54 -0800

    Update +layout.svelte

--- src/routes/build/+layout.svelte
+++ src/routes/build/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import "../app.css";
+	import "../../app.css";
 	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
 	import AppSidebar from "$lib/components/app-sidebar.svelte";
 	import { type Snippet } from "svelte";
